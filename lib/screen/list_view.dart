1c7563a4fd33b90dec983d7d1668deb00cb5a418
3000-2 <psch0493@gmail.com>
2023-02-16 23:05:31 +0900

    ListView example

--- lib/screen/list_view.dart
+++ lib/screen/list_view.dart
@@ -1,15 +1,82 @@
 import 'package:flutter/material.dart';
+import 'package:scrollable_widget/const/colors.dart';
 import 'package:scrollable_widget/layout/main_layout.dart';
 
 class ListViewScreen extends StatelessWidget {
-  const ListViewScreen({Key? key}) : super(key: key);
+  final List<int> numbers = List.generate(100, (index) => index);
+
+  ListViewScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MainLayout(
-        title: 'ListViewScreen',
-        body: ListView(
-          children: [],
-        ));
+    return MainLayout(title: 'ListViewScreen',
+        body: renderSeparated()
+      // renderDefault()
+      //  renderBuilder()
+    );
+  }
+
+  /*
+  * 1. SingleChildScrollView와 비슷하게 동작
+  * */
+  Widget renderDefault() {
+    return ListView(
+      children: numbers
+          .map((number) =>
+          renderContainer(
+              color: rainbowColors[number % rainbowColors.length],
+              index: number))
+          .toList(),
+    );
+  }
+
+  /*
+  * 2. builder 화면에 보이는 것보다 조금 더 그림
+  * */
+  Widget renderBuilder() {
+    return ListView.builder(
+      itemCount: numbers.length,
+      itemBuilder: (context, index) {
+        return renderContainer(
+            color: rainbowColors[index % rainbowColors.length], index: index);
+      },
+    );
+  }
+
+  /*
+  * 3. separated builder와 똑같이 동작하지만 separated가 추가
+  * */
+  Widget renderSeparated() {
+    return ListView.separated(
+      itemCount: numbers.length,
+      itemBuilder: (context, index) {
+        return renderContainer(
+            color: rainbowColors[index % rainbowColors.length], index: index);
+      },
+      separatorBuilder: (BuildContext context, int index) {
+        index += 1;
+        if (index % 5 == 0) {
+          return renderContainer(color: Colors.black, index: index, height: 50);
+        }
+        return Container();
+      },
+    );
+  }
+
+  Widget renderContainer({required Color color, int? index, double? height}) {
+    if (index != null) {
+      print(index);
+    }
+    return Container(
+      height: height == null ? 300 : height,
+      color: color,
+      child: Center(
+        child: Text(
+          index.toString(),
+          style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.w700, fontSize: 30.0),
+        ),
+      ),
+    );
   }
 }
